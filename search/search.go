6945a57ec61760977f43e7900c492a3c092b2567
diamondburned <diamond@arikawa-hi.me>
2023-05-26 01:23:55 -0700

    Simplify PackagesSearcher, add first-class Highlight

--- search/search.go
+++ search/search.go
@@ -1,6 +1,10 @@
 package search
 
-import "context"
+import (
+	"context"
+	"html"
+	"strings"
+)
 
 // TODO: make a search index for s p e e d, maybe use Bleve
 
@@ -8,11 +12,18 @@ import "context"
 type PackagesSearcher interface {
 	// SearchPackages returns a channel of packages that match the given query.
 	// The channel is closed when there are no more results or ctx is canceled.
-	SearchPackages(ctx context.Context, query string) (<-chan SearchedPackage, error)
-	// SearchPackagesRe returns a channel of packages that match the given
-	// regex.
-	// The channel is closed when there are no more results or ctx is canceled.
-	SearchPackagesRe(ctx context.Context, regex string) (<-chan SearchedPackage, error)
+	SearchPackages(ctx context.Context, query string, opts Opts) (<-chan SearchedPackage, error)
+}
+
+// Opts are options for searching.
+type Opts struct {
+	// Highlight is an optional highlighter for this package.
+	// If not nil, it can be used to prehighlight matching terms of all
+	// packages.
+	Highlight HighlightStyle
+	// Regex is whether to use regex matching instead.
+	// If unsupported, it should return an error.
+	Regex bool
 }
 
 // SearchedPackage is a package that was searched for.
@@ -21,3 +32,73 @@ type SearchedPackage struct {
 	// Path is the path to the derivation.
 	Path Path
 }
+
+// HighlightStyle is a style of highlighting.
+type HighlightStyle interface{ isHighlightStyle() }
+
+// HighlightStyleHTML is a style of highlighting that uses HTML.
+type HighlightStyleHTML struct {
+	// Tag is the HTML tag to use for highlighting.
+	// If empty, "mark" is used.
+	Tag string
+	// Attributes is a map of attributes to add to the tag.
+	Attributes map[string]string
+}
+
+func (s HighlightStyleHTML) OpenTag() string {
+	var b strings.Builder
+	b.WriteByte('<')
+
+	if s.Tag != "" {
+		b.WriteString(s.Tag)
+	} else {
+		b.WriteString("mark")
+	}
+
+	for k, v := range s.Attributes {
+		b.WriteByte(' ')
+		b.WriteString(k + `="` + html.EscapeString(v) + `"`)
+	}
+
+	b.WriteByte('>')
+	return b.String()
+}
+
+func (s HighlightStyleHTML) CloseTag() string {
+	if s.Tag != "" {
+		return "</" + s.Tag + ">"
+	}
+	return "</mark>"
+}
+
+// HighlightStyleANSI is a style of highlighting that uses ANSI escape
+// sequences.
+type HighlightStyleANSI struct {
+	// ANSIEscape is the ANSI escape sequence to use for highlighting.
+	// It should be a valid ANSI escape sequence with a prefix of "\x1b".
+	// If empty, red is used.
+	ANSIEscape string
+}
+
+func (s HighlightStyleANSI) ANSIEscapeWithDefault() string {
+	if s.ANSIEscape != "" {
+		return s.ANSIEscape
+	}
+	return DefaultANSIEscapeColor
+}
+
+// DefaultANSIEscapeColor is the default ANSI escape sequence to use for
+// highlighting.
+const DefaultANSIEscapeColor = "\x1b[31m" // FgRed
+
+func (HighlightStyleHTML) isHighlightStyle() {}
+func (HighlightStyleANSI) isHighlightStyle() {}
+
+// Highlights is a list of highlights. Each highlight is a pair of start and end
+// indices.
+type Highlights [][2]int
+
+// HighlightedMap maps an arbitrary value to a list of highlights. When
+// printing, simply check if the value is in the map, and if it is, print the
+// highlights.
+type HighlightedMap map[string]Highlights
